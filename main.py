c73c9703652eb5480bf69d70a0920c1d928370fb
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2021-02-05 12:57:51 +0200

    Perkeliau Loan klase i atskira faila

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-class Loan:
-    def __init__(self, loan_sum, term, interest):
-        self.loan_sum = int(loan_sum)
-        self.term = int(term)
-        self.interest = int(interest)
-        # self.total_interest = self.loan_calc()
-
-
-    def loan_info(self):
-
-        print(f'Paskolos suma: {self.loan_sum}\nPaskolos terminas: {self.term}\n'
-              f'Metinė palūkanų norma: {self.interest}%\n')
-
-    def loan_calc(self):
-        total_interest = round((self.loan_sum*self.interest/100*self.term)/12, 2)
-        refund_sum = self.loan_sum + total_interest
-        return total_interest
-        # print(f'palukanos: {total_interest}\n Is viso {refund_sum}')
-
-
-    def payment_schedule(self):
-        pass
-
-
+from modules.loan import Loan
 
 class main:
     while True:
